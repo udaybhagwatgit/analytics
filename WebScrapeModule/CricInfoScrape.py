a759e5e38489322f70af78a81b76b4db8720232d
Uday Bhagwat <uday.bhagwat7@gmail.com>
2019-04-10 21:22:09 +0530

    Scraped the Batsmen name and score for all matches so far

--- WebScrapeModule/CricInfoScrape.py
+++ WebScrapeModule/CricInfoScrape.py
@@ -3,21 +3,37 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import requests
+import collections
 
 def open_cricinfo_and_begin_mining():
     url = "http://www.espncricinfo.com/scores/series/8048/season/2019/ipl"
-    baseurl = "http://wwww.espncricinfo.com"
+    baseurl = "http://www.espncricinfo.com"
     response = requests.get(url)
     print(response)
     soup = BeautifulSoup(response.content, 'html.parser')
     links = soup.findAll('a', href=True, text='SCORECARD')
     scorecard_links = []
+    #Named Tuple
+    Batsman = collections.namedtuple('Batsman', [
+        'name',
+        'runs'
+    ]
+    )
     for each_link in links:
         scorecard_links.append(baseurl + each_link['href'])
-
-    for each_scorecard_link in scorecard_links:
-        scorecard_response = requests.get(each_scorecard_link)
+        scorecard_link = baseurl + each_link['href']
+        scorecard_response = requests.get(scorecard_link)
         scorecard_obj = BeautifulSoup(scorecard_response.content, 'html.parser')
+        batsmen_list = scorecard_obj.select("div.scorecard-section.batsmen div.wrap.batsmen")
+        for each_batsmen_obj in batsmen_list:
+            each_batsmen = each_batsmen_obj.select("div.cell.batsmen a")
+            batsmen_name = each_batsmen[0].string
+            run_scored_obj = each_batsmen_obj.select("div.cell.runs")
+            run_scored = run_scored_obj[0].string
+            break
+
+    
+
         
 
 open_cricinfo_and_begin_mining()
